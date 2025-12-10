b83b52e803aea9b05950d4728e2fb382155da6ad
tambiarav <1aravindmohan@gmail.com>
2025-12-10 20:45:27 +0530

    debug set to default

--- Backend/backend_recipe/backend_recipe/settings.py
+++ Backend/backend_recipe/backend_recipe/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qq2a7$659zgsi3msm_s^(u#=8cn_!vm@jx98!rhnw8p=br2!p2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
