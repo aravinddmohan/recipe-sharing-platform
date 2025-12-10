675154c4c7a68ce44d2954a7fd0b311d5c94cc7d
tambiarav <1aravindmohan@gmail.com>
2025-12-10 19:34:45 +0530

    switched database to sqlite

--- Backend/backend_recipe/backend_recipe/settings.py
+++ Backend/backend_recipe/backend_recipe/settings.py
@@ -89,15 +89,12 @@ WSGI_APPLICATION = 'backend_recipe.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'recipe_db',#Name of the database created for this project
-        'USER': 'root',#Enter your mysql username
-        'PASSWORD': '',#Enter your mysql password
-        'HOST': 'localhost',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
